5541850220512dfd37760fb06e20e57237bd8178
Lucas Romano <lucas.romano@ngahr.com>
2016-07-13 18:20:36 +0200

    implement basic SPA structure

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,25 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
 import {NavbarComponent} from './navbar/navbar.component';
+import {HomeComponent} from './home.component';
+import {UsersComponent} from './users/users.component';
+import {PostsComponent} from './posts/posts.component';
+
+@RouteConfig([
+  { path: '/', name: 'Home', component: HomeComponent, useAsDefault: true },
+  { path: '/users', name: 'Users', component: UsersComponent },
+  { path: '/posts', name: 'Posts', component: PostsComponent },
+  { path: '/*other', name: 'Other', redirectTo: ['Home'] }
+])
 @Component({
     selector: 'my-app',
     template: `
       <navigation-bar></navigation-bar> 
+      <div class="container">
+        <router-outlet></router-outlet>
+      </div>
     `,
-    directives: [NavbarComponent]
+    directives: [NavbarComponent, ROUTER_DIRECTIVES]
 })
 export class AppComponent { }
