78caa0a703c40b893479104bfc9f15bf83343b60
Lucas Romano <lucas.romano@ngahr.com>
2016-08-01 16:01:28 +0200

    add a spinnerComponent

--- app/posts/posts.component.ts
+++ app/posts/posts.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from 'angular2/core';
 import { PostService } from './post.service';
+import { SpinnerComponent } from '../shared/spinner.component';
 
 @Component({
   templateUrl: 'app/posts/posts.template.html',
+  directives: [SpinnerComponent],
   providers: [PostService]
 })
 export class PostsComponent implements OnInit {
@@ -10,7 +12,7 @@ export class PostsComponent implements OnInit {
   constructor(private _postService:PostService) {}
 
   posts:any[];
-  isLoading: true;
+  isLoading = true;
   
   ngOnInit() {
     this._postService.getPosts().subscribe(posts => {
